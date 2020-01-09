e5fd1e78b903009f9cd631f197ba37e589997353
developereswar <developereswar94@gmail.com>
2020-01-09 10:40:25 +0530

    udpatedViews_v001

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set up a whitelist and check against it:
 
